b309ecd9b5b83d04547566d449993e35cd7346b4
tina naghikhani <tinankh1@gmail.com>
2025-04-17 18:04:20 +0330

    feat:sale chart added to dashboard

--- src/app/dashBoard/page.tsx
+++ src/app/dashBoard/page.tsx
@@ -1,9 +1,10 @@
+import SaleChart from '@/components/salechart/saleChart'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-      
+      <SaleChart/>
     </div>
   )
 }
