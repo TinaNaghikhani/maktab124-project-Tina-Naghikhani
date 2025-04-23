1883b44469ab5b5d151a3218d6cc3c6322eca231
tina naghikhani <tinankh1@gmail.com>
2025-04-23 14:57:21 +0330

    feat:future product and new product added

--- src/components/home/futureProducts/futureProducts.tsx
+++ src/components/home/futureProducts/futureProducts.tsx
@@ -1,10 +1,11 @@
 import Cardcontainer from '@/components/shared/cart/cart'
-import CartComponent from '@/components/shared/cart/cart'
 import React from 'react'
-
+import Image from 'next/image';
+import boxHeader from '@/assets/Picsart_25-04-23_11-42-18-204.jpg'
 export default function FutureProducts() {
   return (
       <div className='w-5/6 px-4 py-10 justify-items-center border-t-[20px] border-[#606c38] rounded-3xl'>
+          <Image src={boxHeader} alt={'boxHeader'} className='w-68 -mt-48' />
       <Cardcontainer/>
     </div>
   )
--- src/components/home/newProducts/newProduct.tsx
+++ src/components/home/newProducts/newProduct.tsx
@@ -1,16 +1,14 @@
 'user client'
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
-import CartComponent from '@/components/shared/cart/cart';
 import Cardcontainer from '@/components/shared/cart/cart';
+import Image from 'next/image';
+import boxHeader from '@/assets/Picsart_25-04-23_11-43-14-032.jpg'
 
 export default function NewProduct() {
 
   return (
     <div className='w-5/6 px-4 py-10 justify-items-center border-t-[20px] border-[#606c38] rounded-3xl'>
+      <Image src={boxHeader} alt={'boxHeader'} className='w-68 -mt-48' />
       <Cardcontainer/>
     </div>
   );
