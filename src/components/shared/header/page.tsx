29f46538d0b3bdc5acf8bfee4832fd9a591143fe
tina naghikhani <tinankh1@gmail.com>
2025-04-23 12:00:56 +0330

    fix:header edited

--- src/components/shared/header/page.tsx
+++ src/components/shared/header/page.tsx
@@ -12,18 +12,18 @@ export default function Header() {
     const { header } = pageLevelLocalization
 
     return (
-        <div className='flex gap-8 justify-between items-baseline px-8 py-2 bg-[#606C38] text-white'>
+        <div className='flex gap-8 justify-between items-center px-8 py-2 bg-[#606C38] text-white'>
             <div className='flex gap-4'>
                 <TiThMenuOutline className="text-3xl mt-4 cursor-pointer hover:border-b" />
                 <nav className='flex gap-6 justify-center items-center p-4 '>
                     <Link href={'/'}>
                         <button className='text-xl cursor-pointer hover:border-b'>{header.home}</button>
                     </Link>
-                    <button className='text-xl cursor-pointer hover:border-b'>{header.fantasy}</button>
+                    {/* <button className='text-xl cursor-pointer hover:border-b'>{header.fantasy}</button>
                     <button className='text-xl cursor-pointer hover:border-b'>{header.litriture}</button>
-                    <button className='text-xl cursor-pointer hover:border-b'>{header.history}</button>
+                    <button className='text-xl cursor-pointer hover:border-b'>{header.history}</button> */}
                     <button className='text-xl cursor-pointer hover:border-b'>{header.juniuor}</button>
-                    <button className='text-xl cursor-pointer hover:border-b'>{header.novel}</button>
+                    {/* <button className='text-xl cursor-pointer hover:border-b'>{header.novel}</button> */}
                     <Link href={'/about'}>
                         <button className='text-xl cursor-pointer hover:border-b'>{header.about}</button>
                     </Link>
@@ -35,7 +35,7 @@ export default function Header() {
             </div>
             <Image src={logo.src}
                 alt="logo"
-                width={150}
+                width={200}
                 height={200}
                 className="-pt-2" />
             <div className='p-4 flex gap-8'>
