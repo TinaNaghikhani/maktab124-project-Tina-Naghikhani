981f74268aeefc4a06b4934cdd8ae09a6cf6e049
tina naghikhani <tinankh1@gmail.com>
2025-04-24 13:18:23 +0330

    fix:single product page

--- src/app/singleProduct/[id]/page.tsx
+++ src/app/singleProduct/[id]/page.tsx
@@ -1,4 +1,6 @@
-import SingleProduct from "@/app/singleProduct/[id]";
+import SingleProduct from "@/app/singleProduct/page";
+import SingleProductComponent from "@/components/singleProducShop/singleProduct";
+import axios from "axios";
 import React from "react";
 
 interface IProps {
@@ -6,26 +8,28 @@ interface IProps {
 }
 
 const BASE_URL="http://api.alikooshesh.ir:3000"
-const api_key=
+const API_KEY=
 "booktinaswuIVzBeQZ98DMmOEmjLenHyKzAbG5UJ4PrAHkD3gV4OnOQvlm6Siz9bKUfKzXjaMicQFeZu21VVmwiwUK5I4qoARsmpvsg5PLu3ee1OzY7XvckHXBmdbOmy"
 
-async function Product({ params }: IProps) {
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) {
+        throw new Error("Access token is missing or expired.");
+    }
     const { id } = await params;
-    const response = await fetch(
-        `${BASE_URL}/api/records/product/${id}`,
-        {
-            headers: {
-               APIKEY:api_key,
-            },
-        }
-    );
-    const result = await response.json();
-    console.log(result);
+    const fetchData = await axios.get(`${BASE_URL}/api/records/product/${id}`, {
+        headers: {
+            "api_key": API_KEY,
+            "Authorization": `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+        },
+    });
+    const productData = fetchData.data;
     return (
         <>
-            <SingleProduct product={result} />
+            <SingleProductComponent product={productData} />
         </>
     );
 }
 
-export default Product;
+export default Page;
